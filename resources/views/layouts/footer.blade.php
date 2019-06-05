c8ada377279d9328b3b9c73eac0dc9d81f0a93c9
brechtbaert <brechtbaert95@gmail.com>
2019-06-05 17:22:02 +0200

    login and database part 2

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,17 @@
+<!-- Footer -->
+<footer class="footer">
+    <div class="container">
+        <div class="text-center">
+            PHP Workshop
+        </div>
+    </div>
+</footer>
+
+
+
+<!-- Footer -->
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Mijn applicatie</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://localhost/includes/custom.css">
+    <link rel="stylesheet" href="http://localhost/includes/footer.css">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="../includes/js/functions.js"></script>
+
+</head>
+<body>
+<div class="container">
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">PHP Workshop</a>
+            </div>
+
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="./index.php">Home</a></li>
+
+
+            </ul>
+            <ul class="nav navbar-right">
+                <li><button id="logOut" class="btn btn-lg btn-primary">Logout</button></li>
+            </ul>
+
+        </div>
+    </nav>
