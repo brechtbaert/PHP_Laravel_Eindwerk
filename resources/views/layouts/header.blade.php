8c41f10ed8b59f93128c229d45032f58326b1bb3
brechtbaert <brechtbaert95@gmail.com>
2019-06-09 15:34:41 +0200

    commit

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,9 +6,9 @@
     <title>Mijn applicatie</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="http://localhost:8000/custom.css">
-    <link rel="stylesheet" href="http://localhost:8000/footer.css">
-    <link rel="stylesheet" href="http://localhost:8000/movieStyle.css">
+    <link rel="stylesheet" href="http://localhost/custom.css">
+    <link rel="stylesheet" href="{{ URL::asset('css/footer.css')}}">
+    <link rel="stylesheet" href="{{ URL::asset('css/movieStyle.css')}}">
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
