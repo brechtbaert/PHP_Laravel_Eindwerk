773d048e523b1a9b45f60fb67a00cbdb84c246d0
brechtbaert <brechtbaert95@gmail.com>
2019-06-11 14:05:59 +0200

    made new routes

--- routes/web.php
+++ routes/web.php
@@ -12,16 +12,28 @@
 */
 
 //get routes
+//movie routes
 Route::get('/','MovieController@showMovies')->name('showMovies');
 Route::get('/editMovie/{movieId}','MovieController@editMovie')->name('editMovie');
 Route::get('/newMovie','MovieController@newMovie')->name('newMovie');
 Route::get('/deleteMovie/{movieId}','MovieController@deleteMovie')->name('deleteMovie');
+
+//director routes
+Route::get('/editDirector/{directorId}','DirectorController@editDirector')->name('editDirector');
+Route::get('/newDirector','DirectorController@newDirector')->name('newDirector');
+route::get('/deleteMovie','DirectorController@deleteDirector')->name('deleteMovie');
 
 
 //authentication routes
 Auth::routes();
 
 //post routes
+//movie routes
 Route::post('/updateMovie','MovieController@updateMovie')->name('updateMovie');
 Route::post('/insertMovie','MovieController@addNewMovie')->name('addNewMovie');
+
+
+//director routes
+Route::post('/updateDirector','DirectorController@updateDirector')->name('updateDirector');
+Route::post('/insertDirector','DirectorController@addNewDirector')->name('addNewDirector');
 
